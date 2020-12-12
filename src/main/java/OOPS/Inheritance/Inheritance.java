42e47628b3ea7fd8794557a1a655e2cc1889dacb
ANKIT OJHA <ojhaankit@outlook.com>
2020-12-12 08:46:37 +0530

    Updated String Notes

--- src/main/java/OOPS/Inheritance/Inheritance.java
+++ src/main/java/OOPS/Inheritance/Inheritance.java
@@ -1,4 +1,27 @@
 package OOPS.Inheritance;
 
 public class Inheritance {
+
+    public static void main(String[] args) {
+        A a = new B();
+        a.sum();
+
+        B b = new B();
+        b.sum();
+
+
+    }
+}
+
+class A {
+    public void sum() {
+        System.out.println("Sum of A");
+    }
+}
+
+class B extends A {
+    @Override
+    public void sum() {
+        System.out.println("Sum of B");
+    }
 }
