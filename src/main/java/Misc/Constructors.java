b136378c0f2259b9a8b8eee944c94231b41d8263
ANKIT OJHA <ojhaankit@outlook.com>
2021-01-04 20:03:00 +0530

    ...

--- src/main/java/Misc/Constructors.java
+++ src/main/java/Misc/Constructors.java
@@ -1,10 +1,14 @@
 package Misc;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Constructors {
 
     public static void main(String[] args) {
         B b1 = new B();
         B b = new B("Hello");
+        WebDriver driver = new ChromeDriver();
     }
 }
 
